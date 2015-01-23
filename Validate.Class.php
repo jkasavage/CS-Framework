7edc7facf21042b8b9fea115b56ed41dbf1dcd40
Joe <joseph@healthclubsystems.com>
2015-01-23 16:22:34 -0600

    Started Validation Class

---
+++ Validate.Class.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CSF\Modules;
+
+/**
+ * Validation - Club Systems Framework
+ * Do NOT modify
+ * 
+ * Copyright Club Systems 2015
+ * @author Joseph Kasavage
+ */
+
+class Validate
+{
+	
+}
